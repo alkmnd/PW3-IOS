8689ccf22a46834c86743d6148ea4ce1ca286a98
teddyzxcv <asfsdg>
2021-10-18 18:46:17 +0300

    Initial Commit

---
+++ nabelova_1PW3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nabelova_1PW3
+//
+//  Created by Наталья Белова on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
