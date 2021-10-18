169f543d401cf7e6b81e315edb297c60f88fe482
alkmnd <asfsdg>
2021-10-19 02:01:25 +0300

    step 4

--- nabelova_1PW3/ViewControllers/TableViewController.swift
+++ nabelova_1PW3/ViewControllers/TableViewController.swift
@@ -12,7 +12,7 @@ class TableViewController: UIViewController {
     private var table: UITableView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.systemYellow
+        view.backgroundColor = UIColor.systemPurple
         setupTableView()
         
         
@@ -26,6 +26,50 @@ class TableViewController: UIViewController {
         table.pin(to: view, .left, .right)
         table.backgroundColor = .white
         self.table = table
+        table.register(EyeCell.self, forCellReuseIdentifier: "eyeCell")
+        table.delegate = self
+        table.dataSource = self
         
     }
 }
+extension TableViewController: UITableViewDelegate {
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection
+                    section: Int) -> Int {300}
+    func numberOfSections(in tableView: UITableView) -> Int {1}
+}
+extension TableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath:
+                    IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "eyeCell",
+            for: indexPath
+        ) as? EyeCell
+        cell?.setupEye()
+        return cell ?? UITableViewCell()
+        }
+}
+final class EyeCell: UITableViewCell {
+    override func prepareForReuse() {
+        for subview in subviews {
+            if subview is UIImageView {
+                subview.removeFromSuperview()
+                
+            }
+        }
+    }
+    func setupEye() {
+        setHeight(to: 50)
+        let image = UIImageView()
+        addSubview(image)
+        image.pinLeft(to: self, Double.random(in: 0...400))
+        image.pinTop(to: self, Double.random(in: -20...40))
+        image.image = UIImage(named: "eye")
+        image.setHeight(to: 20)
+        image.setWidth(to: 30)
+        
+    }
+}
+
